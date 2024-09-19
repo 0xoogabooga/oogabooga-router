89fe358afd0dd6f57cbd341819caefd70e16007d
Bruno Wu <wubruno@outlook.com>
2024-09-19 10:32:33 +0200

    Chore: Rename approvedAddresses to approved

--- contracts/OnlyApproved.sol
+++ contracts/OnlyApproved.sol
@@ -4,24 +4,24 @@ pragma solidity ^0.8.20;
 abstract contract OnlyApproved {
     error NotApprovedAddress(address caller);
 
-    mapping(address => bool) private approvedAddresses;
+    mapping(address => bool) private approved;
 
     modifier onlyApproved() {
-        if (!approvedAddresses[msg.sender]) {
+        if (!approved[msg.sender]) {
             revert NotApprovedAddress(msg.sender);
         }
         _;
     }
 
     function _addApprovedAddress(address _address) internal {
-        approvedAddresses[_address] = true;
+        approved[_address] = true;
     }
 
     function _removeApprovedAddress(address _address) internal {
-        approvedAddresses[_address] = false;
+        approved[_address] = false;
     }
 
     function isApproved(address _address) external view returns (bool) {
-        return approvedAddresses[_address];
+        return approved[_address];
     }
 }
